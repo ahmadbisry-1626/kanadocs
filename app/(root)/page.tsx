d0e91ed41b13d0e31af69dc46f0a89873a49879a
16_shrr <smakarya057@gmail.com>
2024-11-09 23:49:50 +0700

    Add user redirection to documents page if authenticated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,14 @@ import Features from '@/components/landing-page/Features'
 import Footer from '@/components/landing-page/Footer'
 import HeroSection from '@/components/landing-page/HeroSection'
 import NavbarLandingPage from '@/components/landing-page/NavbarLandingPage'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
+const page = async () => {
+    const clerkUser = await currentUser()
+
+    if (clerkUser) redirect('/documents')
     return (
         <div className='overflow-y-auto custom-scrollbar h-[calc(100vh)]'>
             <div className='flex flex-col items-center w-full relative'>
